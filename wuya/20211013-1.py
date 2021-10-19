614a63122b09ddb251fadd0d1e796b4d3f2aca75
wuya-mkjy <69435770+wuya-mkjy@users.noreply.github.com>
2021-10-20 01:00:20 +0800

    Add files via upload

--- wuya/20211013-1.py
+++ wuya/20211013-1.py
@@ -1,5 +1,9 @@
 import cv2
 img=cv2.imread("xixihaha.jpg",cv2.IMREAD_UNCHANGED)
+a=[10,70,130]
+for i in a:
+    for j in a:
+        cv2.rectangle(img,(i,j),(i+50,j+50),(0,255,0),3)
 cv2.imshow("img",img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
