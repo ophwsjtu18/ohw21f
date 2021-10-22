26f7ac147a25c079fea7367c866be57c9affe6ce
wzmsjtu <92438598+wzmsjtu@users.noreply.github.com>
2021-10-22 14:03:40 +0800

    Create code.py

---
+++ wzm/hw1/code.py
@@ -0,0 +1,10 @@
+import numpy as np  
+import cv2  
+img = cv2.imread('cow.jpg',1)  
+for i in range(1,4):  
+　for j in range(1,4):  
+    cv2.rectangle(img,((i-1)*50,0),(i*50,j*50),(0,255,0),3)  
+cv2.imshow('image',img)  
+cv2.waitKey(0)  
+cv2.destroyAllWindows()  
+cv2.imwrite('cow1.jpg',img)
